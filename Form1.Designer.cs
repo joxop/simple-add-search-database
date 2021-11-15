dadb9c767a3b9d314eda23c9a6a9ac0387fbe79c
jay <jayww3@outlook.com>
2021-11-15 01:48:15 -0500

    simple app

---
+++ Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace Project2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pVFCDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.pVFCDataSet = new Project2.PVFCDataSet();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtorder = new System.Windows.Forms.TextBox();
+            this.txtdate = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pVFCDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pVFCDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(403, 41);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 26);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(403, 98);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(148, 26);
+            this.textBox2.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.DataSource = this.pVFCDataSetBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(304, 243);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(518, 545);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "OrderID";
+            this.Column1.HeaderText = "Order ID";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "OrderDate";
+            this.Column2.HeaderText = "Order Date";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "CustomerID";
+            this.Column3.HeaderText = "Customer ID";
+            this.Column3.Name = "Column3";
+            // 
+            // pVFCDataSetBindingSource
+            // 
+            this.pVFCDataSetBindingSource.DataSource = this.pVFCDataSet;
+            this.pVFCDataSetBindingSource.Position = 0;
+            // 
+            // pVFCDataSet
+            // 
+            this.pVFCDataSet.DataSetName = "PVFCDataSet";
+            this.pVFCDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(376, 142);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(213, 83);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Search";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtorder
+            // 
+            this.txtorder.Location = new System.Drawing.Point(1168, 189);
+            this.txtorder.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtorder.Name = "txtorder";
+            this.txtorder.Size = new System.Drawing.Size(247, 26);
+            this.txtorder.TabIndex = 6;
+            // 
+            // txtdate
+            // 
+            this.txtdate.Location = new System.Drawing.Point(1168, 246);
+            this.txtdate.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtdate.Name = "txtdate";
+            this.txtdate.Size = new System.Drawing.Size(247, 26);
+            this.txtdate.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1214, 321);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(213, 83);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Insert";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1066, 195);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "OrderID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1066, 249);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "OrderDate";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(1168, 142);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(247, 28);
+            this.comboBox1.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1067, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "CustomerID";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1636, 1038);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.txtdate);
+            this.Controls.Add(this.txtorder);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pVFCDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pVFCDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource pVFCDataSetBindingSource;
+        private PVFCDataSet pVFCDataSet;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtorder;
+        private System.Windows.Forms.TextBox txtdate;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace Project2
+{
+    public partial class Form1 : Form
+    {
+        SqlTransaction mytxn;
+        SqlConnection myconn = new SqlConnection();
+      
+        public Form1()
+        {
+            InitializeComponent();
+            string path = @"C:\Users\Jay\source\repos\PVFC\PVFC.mdf";
+            myconn.ConnectionString = " Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=" + path + ";Integrated Security=True;Connect Timeout=30";
+            myconn.Open();
+            refreshdata();
+            NewMethod("","");
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //  Establish a connection
+            
+
+
+            //  Make a SQLCommand object
+
+            NewMethod(textBox1.Text,textBox2.Text);
+        }
+
+        private void NewMethod(string Id,string Cust)
+        {
+            //myconn.Open();
+            DataTable mytable = new DataTable();
+            SqlCommand mycommand = new SqlCommand();
+            //  mycommand.CommandText = " Select * from Customer_T where CustomerState = '" + textBox1.Text + "'";
+            mycommand.CommandText = " Select * from Order_T where (OrderID =  @Id OR ''=@id) and CustomerID like  @Cust ";
+          //  mycommand.CommandText = " Select * from Order_T with (READUNCOMMITTED) ";
+            mycommand.Parameters.Add("@Id", SqlDbType.NChar, 20);
+           
+            mycommand.Parameters["@Id"].Value = Id;
+            mycommand.Parameters.Add("@Cust", SqlDbType.NVarChar, 50);
+            mycommand.Parameters["@Cust"].Value = "%" + Cust + "%";
+            mycommand.Connection = myconn;
+
+            //   Create an Adapter  (messenger carrying our request)
+
+            SqlDataAdapter myadapter = new SqlDataAdapter();
+            myadapter.SelectCommand = mycommand;
+
+            //   Fill an internal table
+
+            myadapter.Fill(mytable);
+
+            //   Bind the table to the GUI object
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.DataSource = mytable;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            NewMethod(textBox1.Text, textBox2.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex==0)
+            {
+                MessageBox.Show("Select Customer From List");
+                return;
+            }
+            else if (txtdate.Text=="")
+            {
+                MessageBox.Show("Select Enter Valid Date");
+                return;
+            }
+            else if (txtorder.Text == "")
+            {
+                MessageBox.Show("Select Enter Order ID");
+                return;
+            }
+            
+            
+            DataTable dataTable = new DataTable();
+            SqlCommand mycommand = new SqlCommand();
+            //  mycommand.CommandText = " Select * from Customer_T where CustomerState = '" + textBox1.Text + "'";
+            mycommand.CommandText = " Select * from Order_T where (OrderID =  @Id OR ''=@id) and CustomerID = @Cust ";
+            //  mycommand.CommandText = " Select * from Order_T with (READUNCOMMITTED) ";
+            mycommand.Parameters.Add("@Id", SqlDbType.NChar, 20);
+
+            mycommand.Parameters["@Id"].Value = txtorder.Text;
+            mycommand.Parameters.Add("@Cust", SqlDbType.NVarChar, 50);
+            mycommand.Parameters["@Cust"].Value =  comboBox1.SelectedValue ;
+            mycommand.Connection = myconn;
+
+            //   Create an Adapter  (messenger carrying our request)
+
+            SqlDataAdapter myadapter = new SqlDataAdapter();
+            myadapter.SelectCommand = mycommand;
+            myadapter.Fill(dataTable);
+            if(dataTable.Rows.Count>0)
+            {
+                MessageBox.Show("duplicate");
+            }
+            else
+            {
+                mycommand = new SqlCommand(" insert into  Order_T(OrderID,OrderDate,CustomerID)Values(@Id,@MyDate,@Cust)", myconn);
+               
+                //  mycommand.CommandText = " Select * from Order_T with (READUNCOMMITTED) ";
+                mycommand.Parameters.Add("@Id", SqlDbType.NChar, 20);
+
+                mycommand.Parameters["@Id"].Value = txtorder.Text;
+                mycommand.Parameters.Add("@MyDate", SqlDbType.Date, 20);
+                mycommand.Parameters["@MyDate"].Value = txtdate.Text;
+                mycommand.Parameters.Add("@Cust", SqlDbType.NVarChar, 50);
+                mycommand.Parameters["@Cust"].Value =  comboBox1.SelectedValue;
+                
+                mycommand.ExecuteNonQuery();
+                NewMethod(textBox1.Text,textBox2.Text);
+                MessageBox.Show("Inserted Successfully");
+            }
+           
+        }
+        public void refreshdata()
+        {
+            DataRow dr;
+
+
+            SqlCommand cmd = new SqlCommand("select * from [Customer_T]", myconn);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+
+            dr = dt.NewRow();
+            dr.ItemArray = new object[] { 0, "--Select Customer--" };
+            dt.Rows.InsertAt(dr, 0);
+
+            comboBox1.ValueMember = "CustomerID";
+
+            comboBox1.DisplayMember = "CustomerName";
+            comboBox1.DataSource = dt;
+
+           // myconn.Close();
+        }
+    }
+}
